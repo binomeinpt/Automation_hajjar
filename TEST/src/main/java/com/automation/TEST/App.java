f44ba1802e47278d1337be6423945d641f4bc230
Administrateur <Administrateur@SQLI-HBENSMAIL.SQLI>
2018-01-30 14:18:17 +0000

    HELLO HAJAAAAAAAAAAR

---
+++ TEST/src/main/java/com/automation/TEST/App.java
@@ -0,0 +1,14 @@
+package com.automation.TEST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World123!" );
+    }
+}
